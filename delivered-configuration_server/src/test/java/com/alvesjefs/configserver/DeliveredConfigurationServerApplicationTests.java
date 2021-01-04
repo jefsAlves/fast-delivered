d128a8b46839c2e9f33245329d0289f01e60c9b9
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-01-04 19:03:07 -0300

    Created Configuration Server

---
+++ delivered-configuration_server/src/test/java/com/alvesjefs/configserver/DeliveredConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveredConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
